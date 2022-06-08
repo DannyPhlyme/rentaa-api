d4b409c86fad5724eb506793677dd69ba3790ac3
hp <dannyopeyemi@gmail.com>
2022-06-08 15:49:14 +0100

    latest commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,17 +51,12 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // type: 'postgres',
       // host: process.env.DB_HOST,
       // port: Number(process.env.DB_PORT),
       // username: process.env.DB_USERNAME,
       // password: process.env.DB_PASSWORD,
       // database: process.env.DB,
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
+      url: process.env.DATABASE_URL,
       // ssl: {
       //   rejectUnauthorized: false,
       // },
