12d8acc69f4e7bd64155d27cbd48b582a7092451
DannyPhlyme <dannyopeyemi@gmail.com>
2022-12-30 13:41:52 +0100

    new commit

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,15 +2,16 @@
 set -xe
 
 # Copy compressed artifact file from S3 bucket to webapps/rentaa folder
-aws s3 cp s3://rentaastack-rentaawebappdeployments3bucket-4pf3yd3ozk8/artifact.zip /usr/local/webapps/rentaa/artifact.zip
+aws s3 cp s3://rentaastack-rentaawebappdeployments3bucket-fxwx2l456u6b/artifact.zip /usr/local/webapps/rentaa/artifact.zip
  
 cd /usr/local/webapps/rentaa
 
 # deploy artifact in webapps folder
 unzip artifact.zip
 rm -rf artifact.zip
-rm -rf package-lock.json
-rm -rf node_modules
+# rm -rf package-lock.json
+# rm -rf node_modules
 
-# install application dependencies 
-npm install --legacy-peer-deps
+# install application dependencies
+npm install 
+# npm install --legacy-peer-deps
