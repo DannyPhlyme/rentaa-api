7962929845515ae14046aa2afef03187210937f8
DannyPhlyme <dannyopeyemi@gmail.com>
2022-12-31 20:52:14 +0100

    new commit

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -5,7 +5,11 @@ cd /usr/local/webapps/rentaa
 
 # Start the application server(pm2 managed)
 # start pm2 first
-sudo systemctl start pm2-ubuntu
+# sudo systemctl start pm2-ubuntu
 yarn run start:prod
+
+pm2 startup systemd
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 pm2 save
-sudo systemctl restart pm2-ubuntu
+
+# sudo systemctl restart pm2-ubuntu
