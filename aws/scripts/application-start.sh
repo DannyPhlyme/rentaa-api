9e721c577fa022c678576d7e1698790882a14261
Daniel Olajumoke <42919578+DannyPhlyme@users.noreply.github.com>
2023-01-01 16:53:52 +0100

    Update application-start.sh

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -8,10 +8,10 @@ cd /usr/local/webapps/rentaa
 # sudo systemctl start pm2-ubuntu
 # yarn run start:prod
 
-pm2 startup systemd
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+# pm2 startup systemd
+# sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 
-yarn run start:prod
-pm2 save
+NODE_PORT=3000 pm2 start pm2.yml -n rentaa-app --env production
+# pm2 save
 
 # sudo systemctl restart pm2-ubuntu
