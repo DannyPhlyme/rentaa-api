75ebfe05c0c72b41c79b799478a3e5cb0b5a48d4
Daniel Olajumoke <42919578+DannyPhlyme@users.noreply.github.com>
2023-01-01 16:29:28 +0100

    Update application-start.sh

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -6,10 +6,12 @@ cd /usr/local/webapps/rentaa
 # Start the application server(pm2 managed)
 # start pm2 first
 # sudo systemctl start pm2-ubuntu
-yarn run start:prod
+# yarn run start:prod
 
 pm2 startup systemd
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+
+yarn run start:prod
 pm2 save
 
 # sudo systemctl restart pm2-ubuntu
