9c3a7c39704e1da60b53f50b144bf2e9de60ca20
DannyPhlyme <dannyopeyemi@gmail.com>
2023-01-01 22:49:23 +0100

    new commit

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -14,6 +14,14 @@ cd /usr/local/webapps/rentaa
 # NODE_PORT=3000 pm2 start pm2.yml -n rentaa-app --env production
 # pm2 save
 
+systemctl reload-or-restart pm2-ubuntu
+
+pm2 startup systemd
+env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+
 pm2 start ./dist/main.js -n rentaa-app
+pm2 save
+
+# systemctl start pm2-ubuntu
 
 # sudo systemctl restart pm2-ubuntu
