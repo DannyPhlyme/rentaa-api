6106dbb8579ef8a0629287d6bed4590af4b50eb3
DannyPhlyme <dannyopeyemi@gmail.com>
2023-01-06 20:08:33 +0100

    new commit

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -4,5 +4,5 @@ set -x
 # System control will return either "active" or "inactive".
 pm2_running=$(systemctl is-active pm2-ubuntu)
 if [ "$pm2_running" == "active" ]; then
-    sudo service pm2-ubuntu stop
+    systemctl stop pm2-ubuntu
 fi
--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -xe
 
+# check status of pm2 process 
+# (refer to application stop script)
+systemctl status pm2-ubuntu
+
 # Delete the old directory as needed.
 if [ -d /usr/local/webapps ]; then
     rm -rf /usr/local/webapps/
