b7c46b887294ab67c63117490155bf21e1f1d612
DannyPhlyme <dannyopeyemi@gmail.com>
2023-01-06 20:44:51 +0100

    new commit

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -15,4 +15,8 @@ rm -rf artifact.zip
 yarn install
 
 # sudo systemctl start pm2-ubuntu
-systemctl status pm2-ubuntu
+# systemctl status pm2-ubuntu
+pm2_running=$(systemctl is-active pm2-ubuntu)
+if [ "$pm2_running" == "inactive" ]; then
+    echo "pm2-ubuntu is not running"
+fi
