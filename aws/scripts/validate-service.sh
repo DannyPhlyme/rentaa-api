bff87c63b2b5b9fb53f088a09ac637294ecdede6
DannyPhlyme <dannyopeyemi@gmail.com>
2023-01-09 04:20:19 +0100

    added script to validate that the app is running validate.sh

--- aws/scripts/validate-service.sh
+++ aws/scripts/validate-service.sh
@@ -8,7 +8,7 @@ SLEEP_TIME=3
 # Don't try and verify the certificate; use the --insecure flag.
 for i in `seq 1 $NUMBER_OF_ATTEMPTS`;
 do
-  HTTP_CODE=`curl --insecure --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:3000/hello`
+  HTTP_CODE=`curl --insecure --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:3000/api/v1/hello`
   if [ "$HTTP_CODE" == "200" ]; then
     echo "app server is running."
     exit 0
