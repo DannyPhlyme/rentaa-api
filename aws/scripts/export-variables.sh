ae7232a4606a8cce335aff646c49683d86bf5a4c
DannyPhlyme <dannyopeyemi@gmail.com>
2023-01-09 04:06:43 +0100

    added s3 gadget bucket to export-variables script

--- aws/scripts/export-variables.sh
+++ aws/scripts/export-variables.sh
@@ -9,6 +9,7 @@ APP_PORT=$(aws ssm get-parameter --name "/rentaa/production/app_port" | jq ' .Pa
 SENDGRID_API_KEY=$(aws ssm get-parameter --name "/rentaa/production/sendgrid_api_key" --with-decryption | jq ' .Parameter.Value' | sed -e 's/^"//' -e 's/"$//')
 MIN_PHOTO=$(aws ssm get-parameter --name "/rentaa/production/min_photo" | jq ' .Parameter.Value' | sed -e 's/^"//' -e 's/"$//')
 MAX_PHOTO=$(aws ssm get-parameter --name "/rentaa/production/max_photo" | jq ' .Parameter.Value' | sed -e 's/^"//' -e 's/"$//')
+AWS_PUBLIC_BUCKET_NAME=$(aws ssm get-parameter --name "/rentaa/production/gadgets_s3bucket" | jq ' .Parameter.Value' | sed -e 's/^"//' -e 's/"$//')
 
 cd /usr/local/webapps/rentaa
 
@@ -28,4 +29,5 @@ SECRET_KEY=$SECRET_KEY
 APP_PORT=$APP_PORT
 SENDGRID_API_KEY=$SENDGRID_API_KEY
 MIN_PHOTO=$MIN_PHOTO
-MAX_PHOTO=$MAX_PHOTO" > .env
+MAX_PHOTO=$MAX_PHOTO
+AWS_PUBLIC_BUCKET_NAME=$AWS_PUBLIC_BUCKET_NAME" > .env
